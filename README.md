e5aaf4470d78195501a13f014ccb97a0df55124b
xietian <xietian_job@163.com>
2018-11-16 09:57:10 +0800

    init my project

---
+++ README.md
@@ -0,0 +1,1 @@
+### this is a demo for Vue's practice
---
+++ src/main.js
@@ -0,0 +1,15 @@
+// 入口文件
+import Vue from 'vue'
+
+import app from './App.vue'
+
+import './lib/mui/css/mui.min.css'
+
+import {Header} from 'mint-ui'
+
+Vue.component(Header.name,Header)
+
+var vm = new Vue({
+    el:'#app',
+    render: c => c(app)
+})
