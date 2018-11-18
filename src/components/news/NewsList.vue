caf557618dde91b9c6e5fa226fba5211199e879d
xt1995726 <503634677@qq.com>
2018-11-18 21:31:11 +0800

    newsList

---
+++ src/components/news/NewsList.vue
@@ -0,0 +1,60 @@
+<template>
+    <ul class="mui-table-view">
+        <li class="mui-table-view-cell mui-media" v-for="item in newslist" :key="item.$index">
+            <a href="javascript:;">
+                <img class="mui-media-object mui-pull-left" src="">
+                <div class="mui-media-body">
+                    <h1></h1>
+                    <p class="mui-ellipsis">
+                        <span>发表时间：2018-11-11 00:00:00</span>
+                        <span>点击：0次</span>
+                    </p>
+                </div>
+            </a>
+        </li>
+    </ul>
+</template>
+
+<script>
+import { Toast } from "mint-ui";
+
+export default {
+  data() {
+    return {
+      newslist: []
+    };
+  },
+  created() {
+    this.getNewsList();
+  },
+
+  methods: {
+    getNewsList() {
+      this.$http.get("https://www.apiopen.top/journalismApi").then(result => {
+        if (result.body.code === 200) {
+          this.newslist = result.body.data;
+          console.log(this.newslist);
+        } else {
+          Toast("获取新闻列表失败");
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.mui-table-view {
+  li {
+    h1 {
+      font-size: 14px;
+    }
+    .mui-ellipsis {
+      font-size: 12px;
+      color: #2266ff;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,22 +1,24 @@
-import VueRouter from 'vue-router'
+import VueRouter from "vue-router";
 
-import HomeContainer from './components/tabbar/HomeContainer.vue'
-import MemberContainer from './components/tabbar/MemberContainer.vue'
-import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
-import SearchContainer from './components/tabbar/SearchContainer.vue'
+import HomeContainer from "./components/tabbar/HomeContainer.vue";
+import MemberContainer from "./components/tabbar/MemberContainer.vue";
+import ShopcarContainer from "./components/tabbar/ShopcarContainer.vue";
+import SearchContainer from "./components/tabbar/SearchContainer.vue";
 
+import NewsList from "./components/news/NewsList.vue";
 
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
-    { path:'/', redirect:'/home'},
-    { path: '/home', component: HomeContainer },
-    { path: '/member', component: MemberContainer },
-    { path: '/shopcar', component: ShopcarContainer },
-    { path: '/search', component: SearchContainer },
+    { path: "/", redirect: "/home" },
+    { path: "/home", component: HomeContainer },
+    { path: "/member", component: MemberContainer },
+    { path: "/shopcar", component: ShopcarContainer },
+    { path: "/search", component: SearchContainer },
+    { path: "/home/newslist", component: NewsList }
   ],
-  linkActiveClass: 'mui-active'
-})
+  linkActiveClass: "mui-active"
+});
 
 // 把路由对象暴露出去
-export default router
+export default router;
