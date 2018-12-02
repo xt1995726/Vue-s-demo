7b444a3db4ecfa884ba5d6d3fd87606e4d3d8ef2
xt1995726 <503634677@qq.com>
2018-12-02 23:11:28 +0800

    shopcar

--- src/components/tabbar/ShopcarContainer.vue
+++ src/components/tabbar/ShopcarContainer.vue
@@ -2,18 +2,19 @@
   <div class="shopcar-container">
     <div class="goods-list">
       <!-- 商品列表区域 -->
-      <div class="mui-card">
+      <div class="mui-card" v-for="(item, index) in goodlist" :key="index">
         <div class="mui-card-content">
           <div class="mui-card-content-inner">
-              <mt-switch></mt-switch>
-              <img src="">
-              <div class="info">
-                  <h1>商品名称</h1>
-                  <p>
-                      <span class="price">$2199</span>
-                      <a href="#">删除</a>
-                  </p>
-              </div>
+            <mt-switch></mt-switch>
+            <img :src="item.thumb">
+            <div class="info">
+              <h1>{{item.title}}</h1>
+              <p>
+                <span class="price">${{item.artist_id}}</span>
+                <numbox></numbox>
+                <a href="#">删除</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -30,6 +31,39 @@
 
 
 <script>
+import numbox from '../subcomponents/shopcar_numbox.vue';
+
+export default {
+  data() {
+    return {
+      goodlist: [],
+    }
+  },
+  created() {
+    this.getGoodsList();
+  },
+  methods: {
+    getGoodsList() {
+      var idArr = [];
+      this.$store.state.car.forEach(item => idArr.push(item.id));
+      if (idArr.length <= 0) {
+        return;
+      }
+      idArr.forEach(item => {
+        this.$http.get('https://api.apiopen.top/musicBroadcastingDetails?channelname=' + item).then(res => {
+          if (res.body.code === 200) {
+            // console.log(item);
+            this.goodlist = this.goodlist.concat(res.body.result.songlist[0]);
+          }
+        });
+      });
+
+    }
+  },
+  components: {
+    numbox,
+  },
+}
 </script>
 
 
@@ -37,5 +71,29 @@
 .shopcar-container {
   background-color: #eee;
   overflow: hidden;
+
+  .goods-list {
+    .mui-card-content-inner {
+      display: flex;
+      align-items: center;
+    }
+    img {
+      width: 60px;
+      height: 60px;
+    }
+    h1 {
+      font-size: 13px;
+    }
+    .info {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .price {
+        color: red;
+        font-weight: bold;
+      }
+    }
+  }
 }
 </style>
